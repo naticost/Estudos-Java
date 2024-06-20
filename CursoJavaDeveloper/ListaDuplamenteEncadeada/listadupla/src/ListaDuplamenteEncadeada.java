53f64a49d27eab7408d88079e5392b4375b0c831
NatieleCosta <natisouza806@gmail.com>
2024-06-20 00:27:23 -0300

    add(index)

--- CursoJavaDeveloper/ListaDuplamenteEncadeada/listadupla/src/ListaDuplamenteEncadeada.java
+++ CursoJavaDeveloper/ListaDuplamenteEncadeada/listadupla/src/ListaDuplamenteEncadeada.java
@@ -11,8 +11,57 @@ public class ListaDuplamenteEncadeada<T>{
         this.tamanhoLista = 0;
     }
 
+    public T get(int index){
+        return this.getNo(index).getConteudo();
+    }
+
+    public void add (T elemento){
+        NoDuplo<T> novoNo = new NoDuplo<>(elemento);
+        novoNo.setNoPrevio(null);
+        novoNo.setNoPrevio(ultimoNo);
+        if(primeiroNo == null){
+            primeiroNo = novoNo;
+        }
+        if(ultimoNo != null){
+            ultimoNo.setNoProximo(novoNo);
+        }
+        ultimoNo = novoNo;
+        tamanhoLista++;
+
+    }
+
+    public void add(int index, T elememnto){
+    NoDuplo<T> noAuxiliar = get(index);
+    NoDuplo<T> novoNo = new NoDuplo<>(elemento);
+    novoNo.setNoProximo(noAuxiliar);
+
+
+    if (novoNo.getNoproximo() != null){
+        novoNo.setNoPrevio(noAuxiliar.getNoPrevio());
+        novoNo.getNoProximo().setNoPrevio(novoNo);
+    } else{
+        novoNo.setNoPrevio(ultimoNo);
+        ultimoNo = novoNo;
+    }
+    if(index == 0){
+      primeiroNo = novoNo;
+    }else{
+        novoNo.getNoPrevio().setNoProximo(novoNo);
+    }
+    tamanhoLista++;
+    }
+
+
+    private NoDuplo<T> getNo(int index){
+        NoDuplo<T> noAuxiliar = primeiroNo;
+        for(int i = 0; (i < index) && (noAuxiliar != null); i++){
+            noAuxiliar = noAuxiliar.getNoProximo();
+        }
+        return noAuxiliar;
+    }
+
 
      public int size(){
-         return tamanhoLista;
+         return this.tamanhoLista;
      }
 }
