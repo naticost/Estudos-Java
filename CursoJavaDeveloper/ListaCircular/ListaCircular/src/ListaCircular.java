8f9ab7e253537a7e0bb2e10fee4e7409d6e0813e
NatieleCosta <natisouza806@gmail.com>
2024-06-20 21:42:56 -0300

    método remove

--- CursoJavaDeveloper/ListaCircular/ListaCircular/src/ListaCircular.java
+++ CursoJavaDeveloper/ListaCircular/ListaCircular/src/ListaCircular.java
@@ -1,7 +1,46 @@
 public class ListaCircular<T> {
     private No<T> cabeca;
-    private No<T> calda;
+    private No<T> cauda;
     private int tamanhoLista;
+
+
+    public void remove(int index){
+        if(index >= this.tamanhoLista)
+            throw new IndexOutOfBoundsException("O indice é maior que o tamanho da lista");
+        No<T> noAuxiliar = this.cauda;
+        if(index == 0){
+            this.cauda = this.cauda.getNoProximo();
+            this.cabeca.setNoProximo(this.cauda);
+        } else if(index == 1){
+            this.cauda.setNoProximo(this.cauda.getNoProximo().getNoProximo());
+        }else{
+            for(int i = 0; i < index -1; i++){
+                noAuxiliar = noAuxiliar.getNoProximo();
+            }
+            noAuxiliar.setNoProximo(noAuxiliar.getNoProximo().getNoProximo());
+        }
+        this.tamanhoLista--;
+
+    }
+
+    public T get(int index){
+        return this.getNo(index).getConteudo();
+    }
+
+    private No<T> getNo(int index){
+        if (this.isEmpty())
+            throw new IndexOutOfBoundsException("A lista está vazia");
+        if (index == 0){
+            return this.cauda;
+        }
+
+        No<T> noAuxiliar = this.cauda;
+        for( int i = 0; (i < index) && (noAuxiliar != null); i++){
+            noAuxiliar = noAuxiliar.getNoProximo();
+
+        }
+        return noAuxiliar;
+    }
 
     public boolean isEmpty(){
         return this.tamanhoLista == 0 ? true: false;
