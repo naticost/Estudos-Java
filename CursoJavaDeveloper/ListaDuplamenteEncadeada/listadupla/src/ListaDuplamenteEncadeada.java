34011509c881815c0b008b7b56c802a3257ee8b3
NatieleCosta <natisouza806@gmail.com>
2024-06-20 21:12:09 -0300

    att

--- CursoJavaDeveloper/ListaDuplamenteEncadeada/listadupla/src/ListaDuplamenteEncadeada.java
+++ CursoJavaDeveloper/ListaDuplamenteEncadeada/listadupla/src/ListaDuplamenteEncadeada.java
@@ -51,6 +51,26 @@ public class ListaDuplamenteEncadeada<T>{
     tamanhoLista++;
     }
 
+    public void remove(int index){
+        if(index == 0){
+            primeiroNo = primeiroNo.getNoProximo();
+            if (primeiroNo != null){
+                primeiroNo.setNoPrevio(null);
+            }
+        }else{
+            NoDuplo<T> noAuxiliar = getNo(index);
+            noAuxiliar.getNoPrevio().setNoProximo(noAuxiliar.getNoProximo());
+            if(noAuxiliar!= ultimoNo){
+                noAuxiliar.getNoProximo().setNoPrevio(noAuxiliar.getNoPrevio());
+            }else{
+                ultimoNo = noAuxiliar;
+            }
+        }
+        this.tamanhoLista--;
+
+
+    }
+
 
     private NoDuplo<T> getNo(int index){
         NoDuplo<T> noAuxiliar = primeiroNo;
@@ -64,4 +84,17 @@ public class ListaDuplamenteEncadeada<T>{
      public int size(){
          return this.tamanhoLista;
      }
+
+    @Override
+    public String toString() {
+        String strRetorno = "";
+        NoDuplo<T> noAuxiliar = primeiroNo;
+        for(int i = 0; i <size(); i ++){
+            strRetorno += " [No{conteudo = " + noAuxiliar.getConteudo() + "}] ---->";
+            noAuxiliar = noAuxiliar.getNoProximo();
+
+        }
+        strRetorno += "null";
+        return strRetorno;
+    }
 }
--- CursoJavaDeveloper/ListaDuplamenteEncadeada/listadupla/src/Main.java
+++ CursoJavaDeveloper/ListaDuplamenteEncadeada/listadupla/src/Main.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main (String[] args){
+        ListaDuplamenteEncadeada <String> minhaLista= new ListaDuplamenteEncadeada<>();
+        minhaLista.add("C1");
+        minhaLista.add("C2");
+        minhaLista.add("C3");
+        minhaLista.add("C4");
+        minhaLista.add("C5");
+        minhaLista.add("C6");
+        minhaLista.add("C7");
+
+        System.out.println(minhaLista);
+
+    }
 }
