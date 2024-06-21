0d18770612dad9d5f26e4359405802f509acbc4e
NatieleCosta <natisouza806@gmail.com>
2024-06-20 22:32:41 -0300

    adc do método ToString

--- CursoJavaDeveloper/ListaCircular/ListaCircular/src/Main.java
+++ CursoJavaDeveloper/ListaCircular/ListaCircular/src/Main.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args){
+        ListaCircular<String> minhaListaCircular = new ListaCircular<>();
+        minhaListaCircular.add("C0");
+        System.out.println(minhaListaCircular);
+
+        minhaListaCircular.remove(0);
+        System.out.println(minhaListaCircular);
+
+        minhaListaCircular.add("C1");
+        System.out.println(minhaListaCircular);
+
+        minhaListaCircular.add("C2");
+        minhaListaCircular.add("C3");
+        System.out.println(minhaListaCircular);
+
+        System.out.println(minhaListaCircular.get(0));
+        System.out.println(minhaListaCircular.get(1));
+        System.out.println(minhaListaCircular.get(2));
+        System.out.println(minhaListaCircular.get(3));
+
+    }
 }
