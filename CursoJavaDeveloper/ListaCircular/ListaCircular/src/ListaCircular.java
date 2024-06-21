55e4382b377dee538f1e6087590c01fda852cf31
NatieleCosta <natisouza806@gmail.com>
2024-06-20 21:19:18 -0300

    Empty e SIze

--- CursoJavaDeveloper/ListaCircular/ListaCircular/src/ListaCircular.java
+++ CursoJavaDeveloper/ListaCircular/ListaCircular/src/ListaCircular.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class ListaCircular {
+public class ListaCircular<T> {
+    private No<T> cabeca;
+    private No<T> calda;
+    private int tamanhoLista;
+
+    public boolean isEmpty(){
+        return this.tamanhoLista == 0 ? true: false;
+    }
+
+
+    public int size(){
+        return this.tamanhoLista;
+    }
+
 }
