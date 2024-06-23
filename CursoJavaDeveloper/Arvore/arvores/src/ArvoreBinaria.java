c826ac7eb453cf3cd0bf0e9a837a3935a0b59756
NatieleCosta <natisouza806@gmail.com>
2024-06-22 23:59:59 -0300

    att

--- CursoJavaDeveloper/Arvore/arvores/src/ArvoreBinaria.java
+++ CursoJavaDeveloper/Arvore/arvores/src/ArvoreBinaria.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class ArvoreBinaria {
+public class ArvoreBinaria <T extends Comparable<T>> {
+    private BinNo<T> raiz;
+
+    public ArvoreBinaria(){
+        this.raiz = null;
+    }
+
+    public void inserir(T conteudo){
+        BinNo<T> novNo = new BinNo<>(conteudo);
+        raiz = inserir(raiz, novNo);
+    }
+
+    private BinNo<T> inserir(BinNo<T> atual, BinNo<T> novNo){
+        if(atual==null){
+            return novNo;
+        }else if(novNo.getConteudo().compareTo(atual.getConteudo())< 0){
+            atual.setNoEsq(inserir(atual.getNoEsq(), novNo));
+        }else{
+            atual.setNoDir(inserir(atual.getNoDir(), novNo));
+        }
+        return atual;
+
+    }
 }
+
+
