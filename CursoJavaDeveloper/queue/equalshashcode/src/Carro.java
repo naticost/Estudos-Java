ba4f1d39b22c5a84bbb637dcd5f362363f72c49f
NatieleCosta <natisouza806@gmail.com>
2024-06-24 22:53:08 -0300

    Lista

---
+++ CursoJavaDeveloper/queue/equalshashcode/src/Carro.java
@@ -0,0 +1,38 @@
+import java.util.Objects;
+
+public class Carro {
+    String marca;
+
+    public Carro(String marca) {
+        this.marca = marca;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (object == null || getClass() != object.getClass()) return false;
+        Carro carro = (Carro) object;
+        return Objects.equals(getMarca(), carro.getMarca());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(getMarca());
+
+    }
+
+    @Override
+    public String toString() {
+        return "Carro{" +
+                "marca='" + marca + '\'' +
+                '}';
+    }
+}
