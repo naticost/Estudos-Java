1e000c53f466601af7357b02a0daf8df8a7b92b9
NatieleCosta <natisouza806@gmail.com>
2024-06-27 15:30:24 -0300

    exercicio de LIST e SET

--- CursoJavaDeveloper/AtividadesCollections/list/AtvList/src/temperatura.java
+++ CursoJavaDeveloper/AtividadesCollections/list/AtvList/src/temperatura.java
@@ -1,2 +1,56 @@
-package PACKAGE_NAME;public class temperatura {
+/*Faça um programa que receba a temperatura média os 6 primeiros meses do ano e armazene-as em uma lista.
+Após isto, calcule a média semestral das temperaturas e mostre todas as temperaturas acima desta média,
+e em que mês elas ocorreram (mostrar o mês por extenso: 1 – Janeiro, 2 – Fevereiro e etc).
+*/
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class temperatura {
+    public static void main(String [] args) {
+        Scanner scan = new Scanner(System.in);
+        List<Double> temperaturaSemestral = new ArrayList<>();
+
+        double soma = 0.0;
+        for( int i = 1; i<=6; i++){
+            System.out.print("Qual a temperatura do mês " + i + ": ");
+            double temp = scan.nextDouble();
+            temperaturaSemestral.add(temp);
+            soma += temp;
+        }
+
+        double mediaTemperaturaSemestral = soma/temperaturaSemestral.size();
+        System.out.println("Temperatura Semestral: " + temperaturaSemestral);
+        System.out.println("Média temperatura Semestral: " + mediaTemperaturaSemestral + "ºC");
+        System.out.println("===============================================");
+
+        for(Double temp : temperaturaSemestral){
+            if(temp > mediaTemperaturaSemestral){
+                int index = temperaturaSemestral.indexOf(temp);
+                switch (index){
+                    case 0:
+                        System.out.println((index + 1) + " Janeiro: " + temp + "ºC");
+                        break;
+                    case 1:
+                        System.out.println((index + 1) + " - FEVEREIRO: " + temp+ " oC");
+                        break;
+                    case 2:
+                        System.out.println((index + 1) + " - MARÇO: " + temp+ " oC");
+                        break;
+                    case 3:
+                        System.out.println((index + 1) + " - ABRIL: " + temp+ " oC");
+                        break;
+                    case 4:
+                        System.out.println((index + 1) + " - MAIO: " + temp+ " oC");
+                        break;
+                    case 5:
+                        System.out.println((index + 1) + " - JUNHO: " + temp+ " oC");
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+    }
 }
--- CursoJavaDeveloper/AtividadesCollections/set/Atvarcoiris/src/Exercicio.java
+++ CursoJavaDeveloper/AtividadesCollections/set/Atvarcoiris/src/Exercicio.java
@@ -1,2 +1,63 @@
-package PACKAGE_NAME;public class Exercicio {
+import java.util.*;
+
+/*
+Crie uma conjunto contendo as cores do arco-íris e:
+a) Exiba todas as cores o arco-íris uma abaixo da outra;
+b) A quantidade de cores que o arco-íris tem;
+c) Exiba as cores em ordem alfabética;
+d) Exiba as cores na ordem inversa da que foi informada;
+e) Exiba todas as cores que começam com a letra ”v”;
+f) Remova todas as cores que não começam com a letra “v”;
+g) Limpe o conjunto;
+h) Confira se o conjunto está vazio;
+ */
+public class Exercicio {
+    public static void main (String[] args){
+        System.out.println("Crie um conjunto contendo as cores do arco-íris: ");
+        Set<String> coresArcoIris = new HashSet<>();
+         coresArcoIris.add("violeta");
+         coresArcoIris.add("anil");
+         coresArcoIris.add("azul");
+         coresArcoIris.add("verde");
+         coresArcoIris.add("amarelo");
+         coresArcoIris.add("laranja");
+         coresArcoIris.add("vermelho");
+         System.out.println(coresArcoIris);
+
+         System.out.println("EXIBA TODAS AS CORES DO ARCO-ÍRIS");
+         for(String cor : coresArcoIris){
+             System.out.println(cor);
+         }
+         System.out.println("A QUANTIDADE DE CORES QUE O ARCO-ÍRIS TEM: " + coresArcoIris.size());
+
+         System.out.println("EXIBA AS CORES EM ORDEM ALFABÉTICA: ");
+         Set<String> coresArcoIris2 = new TreeSet<>(coresArcoIris);
+         System.out.println(coresArcoIris2);
+
+         System.out.println("EXIBA AS CORES NA ORDEM INVERSA QUE FOI INFORMADA");
+         Set<String> coresArcoIris3 = new LinkedHashSet<>(
+                 Arrays.asList("violeta", "anil", "azul", "verde", "amarelo", "laranja", "vermelho"));
+         System.out.println(coresArcoIris3);
+         List<String> coresArcoIrisList = new ArrayList<>(coresArcoIris3);
+         Collections.reverse(coresArcoIrisList);
+         System.out.println(coresArcoIrisList);
+
+         System.out.println("EXIBA TODAS AS CORES QUE COMEÇAM COM A LETRA V: ");
+         for(String cor : coresArcoIris){
+             if (cor.startsWith("v" )) System.out.println(cor);
+         }
+
+         System.out.println("REMOVA TODAS AS CORES QUE NÃO COMEÇAM COM A LETRA V: ");
+         Iterator<String> iterator2 = coresArcoIris.iterator();
+         while(iterator2.hasNext()){
+/* O método hasNext() verifica se ainda há elementos na coleção para serem percorridos.
+         Se houver, o loop continua.*/
+             if(!iterator2.next().startsWith("v")) iterator2.remove();
+         }
+         System.out.println("Limpe conjunto: ");
+         coresArcoIris.clear();
+
+         System.out.println("Confira se o conjunto está vazio: " + coresArcoIris.isEmpty());
+
+    }
 }
