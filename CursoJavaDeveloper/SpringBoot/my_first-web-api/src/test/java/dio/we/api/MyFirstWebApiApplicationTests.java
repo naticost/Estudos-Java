514e8f55da516617357d5a53900af5ea8f5fc9e8
NatieleCosta <natisouza806@gmail.com>
2024-07-19 23:07:26 -0300

    att

---
+++ CursoJavaDeveloper/SpringBoot/my_first-web-api/src/test/java/dio/we/api/MyFirstWebApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.we.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFirstWebApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
